d7a08d2f5687561881ab6632849e6c89a66ea162
David Cole <thereckshop@mac.com>
2016-02-10 11:39:28 -0800

    added questions 1-5

---
+++ app.js
@@ -0,0 +1,66 @@
+// JS file for Feb 10 lab
+
+var userName = prompt('Hey! What your name is?');
+console.log('userNAME: ' + userName);
+
+alert('Hello ' + userName + ', We gonna play a game to get to know me better. Please respond to the following questions with a yes or a no please.');
+
+// var answerOne = prompt('Hey ' + userName + ', is my favorite color black?').toUpperCase();
+// console.log('answerOne: ' + answerOne);
+// if (answerOne === 'N' || answerOne === 'NO' || answerOne  === 'NA') {
+//   alert('Damn straight');
+// } else if (answerOne === 'Y' || answerOne === 'YA' || answerOne === 'YES') {
+//   alert('Negative ghostrider');
+// } else {
+//   alert('I SAID ANSWER WITH A YES OR NO!!!');
+// }
+//
+// var answerTwo = prompt('Okay, next question: Was I born in Seattle?').toUpperCase();
+// console.log('answerTwo: ' + answerTwo);
+// if (answerTwo === 'N' || answerTwo === 'NO' || answerTwo === 'NA') {
+//   alert('Darn tootin\'! I was born in Kirkland!');
+// } else if (answerTwo === 'Y' || answerTwo === 'YA' || answerTwo === 'YES') {
+//   alert('Negative ghost rider! I was born in Kirkland.');
+// } else {
+//   alert('I SAID ANSWER WITH A YES OR A NO!!!');
+// }
+//
+// var answerThree = prompt('Right, now for an easy one. Is The Weekend\'s album: "Beauty Behind the Madness" great baby making music acording to Dave?').toUpperCase();
+// console.log('answerThree: ' + answerThree);
+// if (answerThree === 'Y' || answerThree === 'YA' || answerThree === 'YES') {
+//   alert('FUCKING RIGHT! Get it? Fucking, right?');
+// } else if (answerThree === 'N' || answerThree === 'NO' || answerThree === 'NA') {
+//   alert('What\'s wrong with you!?!');
+// } else {
+//   alert('I SAID ANSWER WITH A YES OR A NO!')
+// }
+
+var answerFour = prompt('Does David think it is go time?!').toUpperCase();
+console.log('answerFour: ' + answerFour);
+if (answerFour === 'Y' || answerFour === 'YES' || answerFour === 'YA') {
+  alert('Correct! David always thinks it is go time!');
+} else if (answerFour === 'N' || answerFour === 'NO' || answerFour === 'NA') {
+  alert('WRONG');
+} else {
+  alert('I SAID ANSWER WITH A YES OR A NO!')
+}
+//
+// var answerFive = prompt('Thanks for playing! One last question. Do you think Dave loves you'?).toUpperCase();
+// console.log('answerFive ' + answerFive);
+// if (answerFive === 'Y' || answerFive === 'YA' || answerFive === 'YES'); {
+//   alert('Of course!');
+// } else if (answerFive === 'N' || answerFive === 'NO' || answerFive === 'YA') {
+//   alert('Don\'t be like to boo!');
+// } else {
+//   alert('I SAID ANSWER WITH A YES OR A NO!');
+// }
+//
+// var answerSix = prompt('What is my favorite number?')
+// console.log('answerSix: ' + answerSix);
+// if (answerSix > 7); {
+// alert('Too high!');
+// } else if (answerSix < 7); {
+//   alert('Too low!')
+// } else {
+//   alert('PLEASE ENTER A NUMBER!')
+// }
